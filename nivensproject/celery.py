5695665472194a79fcf336ca66b20e31b21e573c
nivea <nivea@lemeconsultoria.com.br>
2021-10-26 11:12:07 -0300

    adicionando Procfile para Heroku deploy

--- nivensproject/celery.py
+++ nivensproject/celery.py
@@ -1,6 +1,11 @@
 import os
 
 from celery import Celery
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 # Path do módulo
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nivensproject.settings')
 
@@ -9,6 +14,7 @@ app = Celery('nivensproject')
 
 # Configuração do objeto
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'])
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
--- nivensproject/settings.py
+++ nivensproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
+
+django_heroku.settings(locals())
